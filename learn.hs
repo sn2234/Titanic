f51db93b46e027854c15879c7b028ae6fec1cf5a
nobody <nobody@nowhere.com>
2016-01-01 18:30:23 +0200

    Data to matrices

--- learn.hs
+++ learn.hs
@@ -8,4 +8,23 @@ import LogisticRegression
 
 -- Process data set and produce X and Y for logistic regression
 processDataModel :: [DataRec] -> (Matrix Double, Matrix Double)
-processDataModel = undefined
+processDataModel dta =
+	(
+		matrixFromLists $ map dataRecToList dta,
+		transpose $ matrixFromLists $ map (\ x -> [survived x]) dta
+	)
+
+-- X vector only
+dataRecToList :: DataRec -> [Double]
+dataRecToList r =
+		[pclass r, sex r, age r, sibSp r, parch r, fare r, embarked r]
+
+matrixFromLists :: [[a]] -> Matrix a
+matrixFromLists x =
+	matrix (length x) (length (x !! 0)) (\(i, j) -> (x !! (i - 1)) !! (j - 1))
+
+test = do
+	rawData <- parseDataFile "train_pure_cv.csv"
+	case rawData of
+		Right d -> print $ processDataModel d
+		Left err -> print $ "Error: " ++ err
